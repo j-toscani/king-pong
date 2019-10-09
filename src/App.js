7d8ff58405f5f4777c538d8304e1bea1e54206a8
j-toscani <julian_toscani@gmx.de>
2019-10-09 15:00:52 +0200

    include global Style and darkmode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "./GlobalStyle.js";
+import defaultTheme from "./themes/default.js";
+import darkTheme from "./themes/dark.js";
 
-import "./App.css";
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template: 80px 1fr / 1fr;
+  background: ${props => props.theme.main};
+`;
+
+const ButtonStyled = styled.button`
+  background: ${props => props.theme.accent};
+`;
 
 function App() {
+  const [darkmode, setDarkmode] = React.useState(false);
+  function toggleMode() {
+    setDarkmode(!darkmode);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkmode ? darkTheme : defaultTheme}>
+      <Container>
+        <GlobalStyles />
+        <header>Hello, this is header speaking...</header>
+        <div>Hello, this is app Speaking...</div>
+        <footer>Hello this is footer speaking...</footer>
+        <ButtonStyled onClick={toggleMode}>Switch Mode</ButtonStyled>
+      </Container>
+    </ThemeProvider>
   );
 }
 
