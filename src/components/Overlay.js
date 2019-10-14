264e37dca8432945e4c2776d6bc1470b25a3488a
j-toscani <julian_toscani@gmx.de>
2019-10-14 14:08:26 +0200

    add overlay with blur

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -3,10 +3,25 @@ import styled from "styled-components";
 import Settings from "./Settings";
 import RetreatButton from "./RetreatButton";
 
+const Background = styled.div`
+  position: absolute;
+  z-index: -1;
+  width: 100vw;
+  max-width: 360px;
+  min-height: 550px;
+  width: 100%;
+  background: ${props => props.theme.main};
+  filter: blur(4px);
+  opacity: 0.5;
+  padding-top: 50px;
+  padding-bottom: 35px;
+`;
+
 const OverlayContainer = styled.section`
   width: 75vw;
   max-width: 360px;
   min-height: 550px;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -24,13 +39,16 @@ const BottomContainer = styled.div`
 
 function Overlay() {
   return (
-    <OverlayContainer>
-      <Settings></Settings>
-      <BottomContainer>
-        <RetreatButton name={"Profile"}></RetreatButton>
-        <RetreatButton name={"About"}></RetreatButton>
-      </BottomContainer>
-    </OverlayContainer>
+    <>
+      <Background></Background>
+      <OverlayContainer>
+        <Settings></Settings>
+        <BottomContainer>
+          <RetreatButton name={"Profile"}></RetreatButton>
+          <RetreatButton name={"About"}></RetreatButton>
+        </BottomContainer>
+      </OverlayContainer>
+    </>
   );
 }
 
