41cca70be141774786d6362e1ab925481f2e80e0
j-toscani <julian_toscani@gmx.de>
2019-10-18 11:42:13 +0200

    implement: animate ball, listen to input,simple handling of game-end

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import HeartRow from "./HeartRow";
+import { useHistory } from "react-router-dom";
 
 const StyledCanvas = styled.canvas`
   background: ${props => props.theme.accent};
@@ -17,46 +18,83 @@ export default function GameBoard(props) {
   //include draw-function to draw everything once when canvas gets clicked, fiddle around with positioning
   // implement gameloop to let the ball bounce araound
   // implement controles
+  const [ball, changeState] = React.useState({
+    x: 1,
+    y: 1,
+    dx: 1,
+    dy: 1
+  });
+  const [lifes, setLifes] = React.useState(5);
 
   const canvasRef = React.useRef(null);
-  console.log(props.change);
-  React.useEffect(() => {
-    let canvas = canvasRef.current;
-    let ctx = canvas.getContext("2d");
-    let requestId,
-      i = 0,
-      o = 0,
-      w = false,
-      h = true;
-    console.log(i);
 
-    const draw = () => {
-      ctx.clearRect(0, 0, 295, 400);
-      ctx.fillRect(o, i, 10, 10);
-      if (i > 200) {
-        w = true;
-        h = false;
-      }
-      if (w) {
-        o += 1;
-      }
-      if (h) {
-        i += 1;
-      }
-      requestId = requestAnimationFrame(draw);
-    };
-    draw();
-    return () => {
-      cancelAnimationFrame(requestId);
-    };
-  });
+  React.useEffect(() => {
+    if (lifes > 0) {
+      let canvas = canvasRef.current;
+      let ctx = canvas.getContext("2d");
+      let requestId;
+      const board = {
+        x: 295,
+        y: 400
+      };
+      const draw = object => {
+        ctx.clearRect(0, 0, 295, 400);
+        ctx.fillRect(object.x, object.y, 10, 10);
+        requestId = requestAnimationFrame(() => draw(ball));
+        if (object.x > board.x - 10 || object.x < 0) {
+          object.dx *= -1;
+        }
+        if (object.y > board.y - 10 || object.y < 0) {
+          object.dy *= -1;
+          const lifeLost = lifes - 1;
+          setLifes(lifeLost);
+          console.log(lifes);
+        }
+        object.x += object.dx;
+        object.y += object.dy;
+        changeState(object);
+      };
+
+      draw(ball);
+      return () => {
+        cancelAnimationFrame(requestId);
+      };
+    } else {
+      alert("Game Over");
+      handleGameEnding();
+    }
+  }, [lifes, ball.dy]);
+
+  let history = useHistory();
+
+  function handleGameEnding() {
+    history.push(`/select`);
+  }
 
   return (
     <GameContainer>
       <HeartRow p1></HeartRow>
-      <HeartRow p1={false} lifes={4}></HeartRow>
+      <HeartRow p1={false} lifes={lifes}></HeartRow>
 
-      <StyledCanvas width="295" height="400" ref={canvasRef}></StyledCanvas>
+      <StyledCanvas
+        width="295"
+        height="400"
+        ref={canvasRef}
+        onClick={() => {
+          console.log(ball.dy);
+          if (ball.dy === 1) {
+            console.log("hey");
+            changeState(prevState => {
+              return { ...prevState, dy: -1 };
+            });
+          } else {
+            console.log("ho");
+            changeState(prevState => {
+              return { ...prevState, dy: 1 };
+            });
+          }
+        }}
+      ></StyledCanvas>
     </GameContainer>
   );
 }
