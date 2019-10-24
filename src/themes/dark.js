566e85631bb195023f53fc1f9f8b5eca2477afb7
j-toscani <julian_toscani@gmx.de>
2019-10-24 13:31:20 +0200

    add Server

--- src/themes/dark.js
+++
@@ -1,9 +0,0 @@
-const darkTheme = {
-  main: "#595959",
-  contrast: "#262626",
-  accent: "#B4B3B3",
-  brightFont: "#D3D3D3",
-  darkFont: "rgba(0,0,0,0.85)"
-};
-
-export default darkTheme;
--- src/themes/default.js
+++
@@ -1,9 +0,0 @@
-const defaultTheme = {
-  main: "#95ABBF",
-  contrast: "#415A8C",
-  accent: "#F2B604",
-  brightFont: "#FFFFFF",
-  darkFont: "rgba(0,0,0,0.85)"
-};
-
-export default defaultTheme;
