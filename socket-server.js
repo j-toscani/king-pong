32c6d8c3b8b0f0829f0625422472f5be19fc9d4f
j-toscani <julian_toscani@gmx.de>
2019-10-28 10:22:15 +0100

    restructure folders

--- socket-server.js
+++ socket-server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const port = process.env.PORT || 5000;
+const port = process.env.REACT_APP_SERVER_PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
