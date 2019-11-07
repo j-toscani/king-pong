ce5b33fcaf7af760e5b598fb3442a5a7f2865c1a
j-toscani <julian_toscani@gmx.de>
2019-11-07 18:04:12 +0100

    typos

--- socket-server.js
+++ socket-server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 console.log(PORT);
 
 const app = express();
