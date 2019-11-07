3f12817a456c1d2399c58debea17057a0a333409
j-toscani <julian_toscani@gmx.de>
2019-11-07 15:59:10 +0100

    add code snippet

--- socket-server.js
+++ socket-server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 const server = http.createServer(app);
 
 initSocket(server).then(() =>
